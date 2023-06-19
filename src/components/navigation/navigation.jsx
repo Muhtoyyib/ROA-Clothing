a5e39297a9f76744826a06bf552367d81c4523d0
Akande Olalekan Toheeb <akandeolalekantoheeb94@gmail.com>
2023-06-19 02:12:09 +0100

    Building navigation

---
+++ src/components/navigation/navigation.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import CrownSvg from "../../assets/crown-svg";
+import './navigation.styles.scss'
+
+const Navigation = () => {
+  return (
+    <>
+        <div className="navigation">
+            <Link to={`/`} className="logo-container">
+              <CrownSvg />
+            </Link>
+
+            <div className="nav-links-container">
+            <Link to={`/shop`} className="nav-link"> SHOP</Link>
+            </div>
+        </div>
+      
+    </>
+  )
+}
+
+export default Navigation;
