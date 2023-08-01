25c49586c69b1b7c75226388ccd5229ac757613f
Akande Olalekan Toheeb <akandeolalekantoheeb94@gmail.com>
2023-08-01 03:02:33 +0100

    checking commits accross forks

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -61,4 +61,4 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
     return await signInWithEmailAndPassword(auth, email, password);
 }
 
-// Testing still testing
+// Testing still testing testing testing
