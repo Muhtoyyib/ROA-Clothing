99fbe0b5a8ea709a5f52517a88b89b21bbe6357f
Akande Olalekan Toheeb <akandeolalekantoheeb94@gmail.com>
2023-11-07 01:07:54 +0100

    added stripe

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,6 +1,5 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 export const stripePromise = loadStripe(
-    // eslint-disable-next-line no-undef
-    process.env.REACT_APP_STRIPE_PLUBLISHABLE_KEY
+    "pk_test_51O7RXbSHWkYfhcFtHlfnlp8dR1Rsy8sd8PIU3bgpodUGptd1KdXP3NP8a4knXdOW1va0YTYCTx2UZBsINbNoHyxM00BocLBPDM"
 )
